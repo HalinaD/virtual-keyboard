f4c8244f08ec523fc1f4a4a429a33d2c1410e932
halinad <gaxin@list.ru>
2023-05-01 22:42:37 +0300

    feat: add textarea, keys containers and style

--- script.js
+++ script.js
@@ -11,3 +11,49 @@ window.addEventListener("DOMContentLoaded", function () {
       document.body.appendChild(subtitle);   
       keyboard.start();
   });
+
+const keyboard = {
+  properties: {
+    shift: false,
+    capsLock: false,
+    value: "",
+    lang: false
+  },
+  
+  elems: {
+    keys: [],
+    main: null,
+    keysBox: null,
+    language : null
+  },
+  
+  ehandlers: {
+    oninput: null
+  },
+
+    start() {
+        var input = document.createElement('textarea');
+        input.className = 'textarea-field';
+        input.placeholder = 'Press any button...'
+        document.body.appendChild(input);
+        
+      this.elems.keysBox = document.createElement("div");
+      this.elems.main = document.createElement("div");
+     
+      this.elems.main.classList.add("keyboard");
+      this.elems.keysBox.classList.add("keyboard__box");
+      //this.elems.keysBox.appendChild(this.somefunction());
+  
+      this.elems.keys = this.elems.keysBox.querySelectorAll(".keyboard__key");
+  
+      this.elems.main.appendChild(this.elems.keysBox);
+      
+      document.body.appendChild(this.elems.main);
+  
+      document.querySelectorAll(".textarea-field").forEach(element => {
+        element.addEventListener("focus", () => {
+          this.extend(element.value, currentValue => {
+            element.value = currentValue;
+          });
+        });
+      });}}
